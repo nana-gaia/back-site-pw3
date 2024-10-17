11d91dde26e78fded87c5d578b8de622d5d92b64
nana-gaia <geovannacarol.2003@gmail.com>
2024-10-17 05:26:22 -0300

    criação da api

---
+++ app.js
@@ -0,0 +1,14 @@
+// imports
+require('dotenv').config()
+const express = require ('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+
+// open route
+app.get('/', (req, res)=> {
+    res.status(200).json ({ msg: 'Bem-vindo a nossa API!'})
+})
+app.listen(3000)
