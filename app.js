6953bcc01adf407de6fa7f625e11657b395fb87c
nana-gaia <geovannacarol.2003@gmail.com>
2024-10-18 07:28:23 -0300

    conexão com o banco

--- app.js
+++ app.js
@@ -11,4 +11,17 @@ const app = express()
 app.get('/', (req, res)=> {
     res.status(200).json ({ msg: 'Bem-vindo a nossa API!'})
 })
-app.listen(3000)
+
+const bdUser = process.env.BD_USER
+const bdPassword = process.env.BD_PASS
+
+mongoose
+    .connect(
+        `mongodb+srv://${bdUser}:${bdPassword}@back-site.vjsnh.mongodb.net/?retryWrites=true&w=majority&appName=back-site`,
+    )
+    .then(()=>{
+        app.listen(3000)
+        console.log('Conectou ao banco de dados!')
+    })
+
+.catch((err) => console.log('Erro ao conectar ao banco de dados:', err))
